1c6cebc45685a193e097a3301ccfffb70b641edc
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-04-27 17:49:53 -0300

    adiciona link para cadastro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,26 @@
 @extends('layout.app')
 
 @section('body')
-    <h1>New Project</h1>
+<div class="jumbotron bg-light border border-secondary">
+    <div class="row">
+        <div class="card-deck">
+            <div class="card border border-primary">
+                <div class="card-body">
+                    <h5 class="card-title">Register</h5>
+                    <p class="card-text">Cadastre todos os seus produtos registrando
+                        previamente as categorias.</p>
+                    <a href="/products" class="btn btn-primary">Cadastrar</a>
+                </div>
+            </div>
+            <div class="card border border-primary">
+                <div class="card-body">
+                    <h5 class="card-title">Register</h5>
+                    <p class="card-text">Cadastre todos os seus produtos registrando
+                        previamente as categorias.</p>
+                    <a href="/products" class="btn btn-primary">Cadastrar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
