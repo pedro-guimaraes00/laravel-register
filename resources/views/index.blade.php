339829a386ece0717854d3477bc58e45c7e9a5a9
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-04-27 17:15:50 -0300

    cria app.blade e rota index do projeto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+
+@section('body')
+    <h1>New Project</h1>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Cadastro de Produtos</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <main role="main">
+                @hasSection ('body')
+                    @yield('body')
+                @endif
+            </main>
+        </div>
+
+    <script src="{{ asset("js/app.js") }}" type="text/javascript"></script>    
+    </body>
+</html>
