aaacc8b46eaecd61053b3354e91de72f23757ba9
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-05-16 20:13:30 -0300

    corrige layout da index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,15 @@
 @extends('layout.app', ["current" => "index"])
 
 @section('body')
+
 <div class="jumbotron bg-light border border-secondary">
-    <div class="row">
-        <div class="card-deck">
-            <div class="card border border-primary">
-                <div class="card-body">
-                    <h5 class="card-title">Cadastro de produtos</h5>
-                    <p class="card-text">Cadastre previamente os departamentos dos produtos.</p>
-                    <a href="/products/new" class="btn btn-primary">Cadastrar</a>
-                </div>
-            </div>
-            <div class="card border border-primary">
-                <div class="card-body">
-                    <h5 class="card-title">Cadastro de Departamentos</h5>
-                    <p class="card-text">Cadastre os departamentos dos produtos inseridos.</p>
-                    <a href="/departments/new" class="btn btn-primary">Cadastrar</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <h1 class="cover-heading">Olá, seja bem-vindo.</h1>
+        <p class="lead">Este é o <b class="font-italic">Stock Manager</b>. Escolha abaixo qual
+            setor deseja <b class="font-weight-bold">cadastrar</b>:</p>
+        <p class="lead">
+          <a href="/products/new" class="btn btn-lg btn-primary">Produtos</a>
+          <a href="/departments/new" class="btn btn-lg btn-primary">Departamentos</a>
+        </p>
+</div>       
+
 @endsection
