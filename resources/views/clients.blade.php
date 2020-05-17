7f5136e8de934a7b141e80f8b4af09394b94c873
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-05-17 18:24:30 -0300

    implementa view clients

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -1,5 +1,36 @@
 @extends('layout.app', ['current' => 'clients'])
 
 @section('body')
-    <h1>Clientes</h1>
+
+        <div class="card border">
+            <div class="card-body">
+            <h5 class="card-title">Cadastro de Clientes</h5>
+                <table class="table table-ordered table-hover" id="tableClients">
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Nome</th>
+                            <th>Idade</th>
+                            <th>Endereço</th>
+                            <th>E-mail</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($clients as $cli)
+                        <tr>
+                            <td>{{$cli->id}}</td>
+                            <td>{{$cli->name}}</td>
+                            <td>{{$cli->age}}</td>
+                            <td>{{$cli->adress}}</td>
+                            <td>{{$cli->email}}</td>
+                            <td>
+                                <a href="/clients/edit/{{$cli->id}}" class="btn btn-sm btn-primary">Editar</a>
+                                <a href="/clients/delete/{{$cli->id}}" class="btn btn-sm btn-danger">Apagar</a>
+                            </td>
+                        </tr>
+                    @endforeach    
+                    </tbody>
+                </table>
+        </div>
+
 @endsection
