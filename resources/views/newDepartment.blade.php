e6381aa9772cac6b6f28fcfe0bce8cf36b5d07fc
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-05-21 18:53:12 -0300

    adiciona validação campo-a-campo, substituindo por validação antiga

--- resources/views/newDepartment.blade.php
+++ resources/views/newDepartment.blade.php
@@ -4,26 +4,41 @@
 
     <div class="card border">
         <div class="card-body">
-            <form action="/departments" method="POST">
+            <form class="needs-validation" action="/departments" method="POST" novalidate>
                 @csrf
                 <div class="form-group">
                     <label for="nameDepartment">Novo Departamento</label>
                     <input type="text" class="form-control" name="nameDepartment" id="nameDepartment"
-                        placeholder="Departamento">
+                        placeholder="Departamento" required>
+                    <div class="invalid-feedback">
+                        O campo é obrigatório
+                    </div>    
                 </div>
                 <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
                 <button type="cancel" class="btn btn-danger btn-sm">Cancelar</button>
             </form>
         </div>
-        @if ($errors->any())
-            <div class="card-footer">
-                @foreach ($errors->all() as $error)
-                    <div class="alert alert-danger" role="alert">
-                        {{ $error }}
-                    </div>    
-                @endforeach
-            </div>
-        @endif
     </div>
+
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+          'use strict';
+          window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+              form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+              }, false);
+            });
+          }, false);
+        })();
+        </script>
 
 @endsection
--- resources/views/newProduct.blade.php
+++ resources/views/newProduct.blade.php
@@ -4,38 +4,65 @@
 
     <div class="card border">
         <div class="card-body">
-            <form action="/products" method="POST">
+            <form class="needs-validation" action="/products" method="POST" novalidate>
                 @csrf
-                <div class="form-group">
-                <label for="nameProduct">Novo Produto</label>
-                    <select class="form-control form-control-sm" name="department" id="department">
-                    <option selected>Defina o departamento</option>
-                        @foreach ($department as $dept)
-                            <option value="{{ $dept->id }}">{{ $dept->name }}</option>
-                        @endforeach    
-                    </select>
-                    <input type="text" class="form-control" name="nameProduct" id="nameProduct"
-                    placeholder="Produto">
-                    <input type="text" class="form-control" name="stockProduct" id="stockProduct"
-                    placeholder="Quantidade em estoque">
-                    <input type="text" class="form-control" name="priceProduct" id="priceProduct"
-                    placeholder="Preço">
-                </div>
+                    <div class="form-group">
+                    <label for="nameProduct">Novo Produto</label>
+                        <select class="custom-select custom-select-sm" name="department" id="department" required>
+                            <option selected>Defina o departamento</option>
+                                @foreach ($department as $dept)
+                                    <option value="{{ $dept->id }}">{{ $dept->name }}</option>
+                                @endforeach    
+                        </select>
+                        <div class="invalid-feedback">O campo é obrigatório.</div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="nameProduct" id="nameProduct"
+                        placeholder="Produto" required>
+                            <div class="invalid-feedback">
+                                O campo é obrigatório.
+                            </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="stockProduct" id="stockProduct"
+                        placeholder="Quantidade em estoque" required>
+                            <div class="invalid-feedback">
+                                O campo é obrigatório.
+                            </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="priceProduct" id="priceProduct"
+                        placeholder="Preço" required>
+                            <div class="invalid-feedback">
+                                O campo é obrigatório.
+                            </div>
+                    </div>
                 <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
                 <button type="cancel" class="btn btn-danger btn-sm">Cancelar</button>
             </form>
         </div>
-        @if($errors->any())
-            <div class="card-footer">
-                @foreach ($errors->all() as $error)
-                    <div class="alert alert-danger">
-                        {{ $error }}
-                    </div>
-                @endforeach
-            </div>
-        @endif
     </div>
 
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+          'use strict';
+          window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+              form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+              }, false);
+            });
+          }, false);
+        })();
+        </script>
 
 
 @endsection
